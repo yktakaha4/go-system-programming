59e2ecd62be72f4b168bed00ca78409fdf813a9e
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-06-27 21:58:24 +0000

    add net.Dial

---
+++ 2_4_5.go
@@ -0,0 +1,17 @@
+// go:build ignore
+package main
+
+import (
+	"io"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "example.com:80")
+	if err != nil {
+		panic(err)
+	}
+	io.WriteString(conn, "GET / HTTP/1.0\r\nHost: example.com\r\n\r\n")
+	io.Copy(os.Stdout, conn)
+}
